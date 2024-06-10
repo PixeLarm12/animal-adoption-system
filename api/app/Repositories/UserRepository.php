a5001f68d6110b4d2effbb1dfbb909590095b231
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-06-10 11:51:01 -0300

    Update UserRepository

--- api/app/Repositories/UserRepository.php
+++ api/app/Repositories/UserRepository.php
@@ -2,11 +2,27 @@
 
 namespace App\Repositories;
 
+use App\Models\Phone;
 use App\Models\User;
 
 class UserRepository extends BaseRepository
 {
     public function __construct(User $model) {
         parent::__construct($model);
+    }
+
+    public function save(array $data): User
+    {
+        $user = User::create($data);
+
+        if($data['phones']) {
+            $user->phones()->createMany($data['phones']);
+        }
+
+        if($data['addresses']) {
+            $user->addresses()->createMany($data['addresses']);
+        }
+
+        return $user->with(["phones", "addresses"]);
     }
 }
