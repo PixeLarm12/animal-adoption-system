0ef88f9f0e9293495cc95005a7cc9088380eb3cf
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-06-10 11:40:27 -0300

    Fixed Addresses and Phones relations

--- api/app/Models/Address.php
+++ api/app/Models/Address.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
-    use HasFactory;
+    protected $table = "addresses";
+
+    protected $fillable = [
+        'street',
+        'number',
+        'zipcode',
+        'neighborhood',
+        'complement',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id', 'person_id');
+    }
 }
--- api/app/Models/Phone.php
+++ api/app/Models/Phone.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Phone extends Model
 {
-    use HasFactory;
+    protected $table = "phones";
+
+    protected $fillable = [
+        'area',
+        'phone',
+        'person_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id', 'person_id');
+    }
 }
