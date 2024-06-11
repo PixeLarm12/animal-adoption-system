87b4783c4d36bc7a3937e7673ecddd8dc354a2a0
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-06-10 22:31:36 -0300

    Added basic Home page

--- front/src/pages/Home.vue
+++ front/src/pages/Home.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
-        <h1 class="text-lg text-red-600">Home Page</h1>
+    <div class="min-h-[100vh] w-screen flex flex-col justify-center items-center">
+        <div class="w-screen h-[50vh] flex flex-row justify-center items-center">
+            <router-link to="/catalog" class="w-3/12 h-44 flex flex-col justify-center items-center border bg-orange-500 text-2xl">
+                Catalog
+            </router-link>
+        </div>
     </div>
 </template>
