ca6a368e15d34064b2de44125332eac418ad72ae
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-06-10 22:34:00 -0300

    Added correct route

--- front/src/pages/Home.vue
+++ front/src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="min-h-[100vh] w-screen flex flex-col justify-center items-center">
         <div class="w-screen h-[50vh] flex flex-row justify-center items-center">
-            <router-link to="/catalog" class="w-3/12 h-44 flex flex-col justify-center items-center border bg-orange-500 text-2xl">
+            <router-link to="/catalogs" class="w-3/12 h-44 flex flex-col justify-center items-center border bg-orange-500 text-2xl">
                 Catalog
             </router-link>
         </div>
