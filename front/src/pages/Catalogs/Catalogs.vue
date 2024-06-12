d807f671c63c24e3fb7f5bf1fe9ed4061888a7c8
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-06-12 11:35:41 -0300

    Added axios for catalog routines

--- front/src/pages/Catalogs/Catalogs.vue
+++ front/src/pages/Catalogs/Catalogs.vue
@@ -6,13 +6,13 @@
             <button @click="showTable = !showTable" class="default-button mb-5">{{ getShowButtonTitle }}</button>
         </div>
         
-        <div v-show="!showTable" class="w-8/12 flex flex-row justify-center items-center gap-4 flex-wrap">
-            <router-link to="/catalogs/detail/1" class="bg-catalog-card">
-                <h2>See catalog 1</h2>
+        <div v-show="!showTable && catalogs" class="w-8/12 flex flex-row justify-center items-center gap-4 flex-wrap">
+            <router-link v-for="catalog in catalogs" :key="catalog.id" :to="'/catalogs/edit/' + catalog.id" class="bg-catalog-card">
+                <h2 class="text-bold text-xl">{{ catalog.title }}</h2>
             </router-link>
         </div>
 
-        <div v-show="showTable" class="w-8/12 flex flex-row justify-center items-center gap-4 flex-wrap">
+        <div v-show="showTable && catalogs" class="w-8/12 flex flex-row justify-center items-center gap-4">
             <table-template :headers="tableHeaders">
                 <tr v-for="catalog in catalogs" :key="catalog.id" class="w-full border border-black divide-x-2 divide-black">
                     <td class="lg:pl-3 pl-1 h-10 w-4/12">
@@ -30,47 +30,56 @@
                 </tr>  
             </table-template>
         </div>
+
+        <div v-if="!catalogs" class="w-8/12 flex justify-center items-center">
+            <h3 class="text-xl text-red-600 font-bold">Sorry, but we don't have any active catalogs to show...</h3>
+        </div>
     </page-template>
 </template>
 
 <script>
-import PageTemplate from "../../components/PageTemplate.vue"
+import axios from 'axios';
+import PageTemplate from "../../components/PageTemplate.vue";
 import TableTemplate from '../../components/Utils/TableTemplate.vue';
 
 export default {
+    beforeMount() {
+        axios.get('http://localhost/api/catalogs')
+            .then(response => this.catalogs = response.data)
+            .catch(error => console.log(`Error: ${error}`));
+    },
+
     components: {
         PageTemplate,
         TableTemplate
     },
+
     computed: {
         getShowButtonTitle() {
             return this.showTable ? "Card view" : "Table view"; 
         }
     },
+
     data() {
         return {
             showTable: false,
-            catalogs: [
-                {
-                    id: 1,
-                    title: "Catalog 1",
-                    description: "New catalog description 1",
-                },
-                {
-                    id: 2,
-                    title: "Catalog 2",
-                    description: "New catalog description 2",
-                },
-            ],
+            catalogs: [],
             tableHeaders: [
                 "Title",
                 "Description",
             ]
         }
     },
+
     methods: {
         remove(id) {
-            console.log(`This id: ${id} needs to be removed!!!`);
+            axios.delete(`http://localhost/api/catalogs/${id}`)
+                .then((response) => {
+                    if(response) {
+                        this.$router.go(this.$router.currentRoute);
+                    }
+                })
+                .catch(error => console.log(`Error: ${error}`));
         }
     }
 }
